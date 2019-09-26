fbf317b22a13e710155c53b0b687934613f64981
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-09-26 17:44:51 +0800

    real first deisgn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,70 +8,126 @@
 
         <!-- Fonts -->
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
         {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
         <!-- Styles -->
 
+        <style>
+        .material-icons.md-36 { font-size: 32px; }
+        </style>
     </head>
-    <body class="flex h-screen">
+    <body class="flex h-screen font-sans ">
 
         <side-bar class="w-2/12 flex flex-col bg-indigo-800">
-            <top-bar class="h-24  border-b-1  flex items-center border-black">
-                    <h1 class="text-white text-2xl font-sans m-auto">Dream Team</h1>
+            <top-bar class="h-24 flex items-center border-b border-indigo-700 py-5">
+                    <img class="w-7/12 mx-auto mt-3" src="https://fontmeme.com/permalink/190926/504d6783995232cf36f03478b4e00769.png" alt="netflix-font" border="0">
             </top-bar>
             <menu-bar class="h-full flex flex-col ">
-                    <a href="#" class="text-lg font-semibold mb-10 mt-12 ml-24 text-white">Overview</a>
-                    <a href="#" class="text-lg font-semibold mb-10 ml-24 text-white">Achievements</a>
-                    <a href="#" class="text-lg font-semibold mb-10 ml-24 text-white">Tournaments</a>
-                    <a href="#" class="text-lg font-semibold mb-10 ml-24 text-white">Teams</a>
-                    <a href="#" class="text-lg font-semibold text-white ml-24">Scrims</a>
+                    <a href="#" class="text-lg font-semibold mb-10 mt-12 ml-16 text-purple-400">
+                            <i class="material-icons  align-middle">assessment</i>
+                        <span class="align-middle ml-2">Overview</span>
+                    </a>
+                    <a href="#" class="text-lg font-semibold mb-10 ml-16 text-white"><i class="material-icons align-middle">
+                            emoji_events
+                            </i> <span class="align-middle  ml-2">Achievements</span>
+                    </a>
+                    <a href="#" class="text-lg font-semibold mb-10 ml-16 text-purple-400">
+                            <i class="material-icons  align-middle">videogame_asset</i>
+                        <span class="align-middle ml-2">Tournaments</span>
+                    </a>
+                    <a href="#" class="text-lg font-semibold mb-10 ml-16 text-purple-400">
+                            <i class="material-icons  align-middle">group</i>
+                        <span class="align-middle ml-2">Teams</span>
+                    </a>
+                    <a href="#" class="text-lg font-semibold mb-10 ml-16 text-purple-400">
+                            <i class="material-icons  align-middle">sports_kabaddi</i>
+                        <span class="align-middle ml-2">Scrims</span>
+                    </a>
+
             </menu-bar>
-
-
-        </side-bar>
-
-        <main-div class="w-10/12 flex flex-col">
-
-                    <header class="h-24 flex justify-between">
-                    <div class="w-8 flex items-center ml-12">
-                        <svg  class="w-8" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu">
-                        <line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line>
-                        <line x1="3" y1="18" x2="21" y2="18"></line></svg>
-                    </div>
-                    <div class="flex items-center justify-around mr-12">
-                        {{-- <h1 class=" text-2xl font-sans m-auto">Dream Team</h1> --}}
-                        <div class="mx-3">
-                            <svg class="w-6" xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
-                            <path d="M22 17H2a3 3 0 0 0 3-3V9a7 7 0 0 1 14 0v5a3 3 0 0 0 3 3zm-8.27 4a2 2 0 0 1-3.46 0"></path>
-                            </svg>
-                        </div>
-                        <div class="mx-3">
-                            <img  class="rounded-full h-12 w-12" src="{{Auth::user()->accounts->avatar_url}}" alt="">
-                        </div>
-                        <div class="mx-3">
-                            <svg class="w-8" width="7px" height="20px" viewBox="0 0 7 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                <!-- Generator: Sketch 3.8.1 (29687) - http://www.bohemiancoding.com/sketch -->
-                                <title>menu_option [#1375]</title>
-                                <desc>Created with Sketch.</desc>
-                                <defs></defs>
-                                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                    <g id="Dribbble-Light-Preview" transform="translate(-266.000000, -800.000000)" fill="#000000">
-                                        <g id="icons" transform="translate(56.000000, 160.000000)">
-                                            <path d="M212.333333,658 L214.666667,658 L214.666667,656 L212.333333,656 L212.333333,658 Z M210,660 L217,660 L217,654 L210,654 L210,660 Z M212.333333,651 L214.666667,651 L214.666667,649 L212.333333,649 L212.333333,651 Z M210,653 L217,653 L217,647 L210,647 L210,653 Z M212.333333,644 L214.666667,644 L214.666667,642 L212.333333,642 L212.333333,644 Z M210,646 L217,646 L217,640 L210,640 L210,646 Z" id="menu_option-[#1375]"></path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>
-                    </div>
-                </header>
-
-            <content-div class="bg-gray-200 h-screen">
-                <div class="container mx-auto mt-16">
-                content
+            <div class="flex justify-center items-center py-6 pr-6 cursor-pointer bg-purple-800 border-t border-indigo-700 ">
+                    <i class="material-icons mr-3 align-middle font-semibold text-white">
+                            power_settings_new
+                            </i>
+                    <span class="font-semibold text-white text-xl align-middle">
+                        log out
+                    </span>
             </div>
-            </content-div>
+        </side-bar>
+        <main-div class="w-10/12 flex flex-col">
+                    <header class="h-24 flex justify-between border-b-2 border-gray-300 shadow-xl">
+                        <div class="w-8 flex items-center ml-12">
+                                <i class="material-icons md-36 cursor-pointer">
+                                        menu_open
+                                        </i>
+                        </div>
+                        <div class="flex items-center justify-around mr-12">
+                            {{-- <h1 class=" text-2xl font-sans m-auto">Dream Team</h1> --}}
+                            <div class="mx-3">
+                                    <i class="material-icons mt-1 text-yellow-500 md-36 cursor-pointer">
+                                            notifications_none
+                                            </i>
+                            </div>
+                            <div class="mx-3">
+                                <img  class="rounded-full h-12 w-12 cursor-pointer" src="{{Auth::user()->accounts->avatar_url}}" alt="">
+                            </div>
+                            <div class="mx-3">
+                                    <i class="material-icons md-36 cursor-pointer">
+                                            more_horiz
+                                            </i>
+                            </div>
+                        </div>
+                    </header>
+                    <content-div class="bg-gray-200 h-screen">
+                        <div class="container mx-auto mt-16">
+                            <div class="flex">
+                                    <div class="w-2/3">
+                                        <div class="bg-white shadow-lg text-center rounded ">
+                                            <table class="border-collapse w-full">
+                                                <thead>
+                                                    <th class="uppercase border-b border-gray-400 py-4 ">#</th>
+                                                    <th class="uppercase border-b border-gray-400 py-4 ">Tournament Name</th>
+                                                    <th class="uppercase border-b border-gray-400 py-4 ">Team</th>
+                                                    <th class="uppercase border-b border-gray-400 py-4 ">Place</th>
+                                                    <th class="uppercase border-b border-gray-400 py-4 ">Date</th>
+                                                </thead>
+                                                <tbody>
+                                                    <tr class="hover:bg-gray-200">
+                                                        <td class="py-4 px-6 border-b border-gray-300">1</td>
+                                                        <td class="py-4 px-6 border-b border-gray-300">KL Major 2</td>
+                                                        <td class="py-4 px-6 border-b border-gray-300">Liquid</td>
+                                                        <td class="py-4 px-6 border-b border-gray-300">Champion</td>
+                                                        <td class="py-4 px-6 border-b border-gray-300">12/02/2012</td>
+                                                    </tr>
+                                                    <tr class="hover:bg-gray-200">
+                                                            <td class="py-4 px-6 border-b border-gray-300">2</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">KL Major 2</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">Liquid</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">Champion</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">12/02/2012</td>
+                                                        </tr>
+                                                        <tr class="hover:bg-gray-200">
+                                                            <td class="py-4 px-6 border-b border-gray-300">3</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">KL Major 2</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">Liquid</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">Champion</td>
+                                                            <td class="py-4 px-6 border-b border-gray-300">12/02/2012</td>
+                                                        </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                    <div class="w-auto ml-24 mt-1">
+                                            <button class="bg-transparent hover:bg-indigo-700 text-indigo-800
+                                            font-semibold hover:text-white py-2 px-4 border border-indigo-800
+                                            hover:border-transparent rounded">
+                                                New Achievement
+                                            </button>
+                                        </div>
+                            </div>
+                        </div>
+                    </content-div>
         </main-div>
 
 
