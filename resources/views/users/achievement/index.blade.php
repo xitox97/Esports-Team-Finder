2b78379429e4b4054093f61337bac9e86c7f8089
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-09-28 22:50:34 +0800

    done achievements

--- resources/views/users/achievement/index.blade.php
+++ resources/views/users/achievement/index.blade.php
@@ -8,15 +8,16 @@
 <div class="container mx-auto mt-16">
         <div class="flex">
 <div class="w-5/6 ml-16">
-        <div class="bg-white shadow-lg text-center rounded p-4">
-                <p class="text-xl  ml-4 font-bold underline pb-2 uppercase">past achievements</p>
-            <table class="border-collapse w-full ">
-                <thead>
-                    <th class="uppercase border-b border-gray-400 py-4 ">#</th>
-                    <th class="uppercase border-b border-gray-400 py-4 ">Tournament Name</th>
-                    <th class="uppercase border-b border-gray-400 py-4 ">Team</th>
-                    <th class="uppercase border-b border-gray-400 py-4 ">Place</th>
-                    <th class="uppercase border-b border-gray-400 py-4 ">Date</th>
+    <p class="text-2xl  ml-1 font-bold  pb-4 capitalize">past achievements</p>
+        <div class="bg-white shadow-lg text-center rounded-lg px-4 pb-6 pt-5">
+                {{-- <p class="text-xl  ml-4 font-bold underline pb-2 uppercase">past achievements</p> --}}
+            <table class="border-collapse w-full">
+                <thead class="text-gray-600">
+                    <th class="capitalize border-b border-gray-300 py-4 ">#</th>
+                    <th class="capitalize border-b border-gray-300 py-4 ">Tournament Name</th>
+                    <th class="capitalize border-b border-gray-300 py-4 ">Team</th>
+                    <th class="capitalize border-b border-gray-300 py-4 ">Place</th>
+                    <th class="capitalize border-b border-gray-300 py-4 ">Date</th>
                 </thead>
                 <tbody class="text-center">
                     @forelse($users->achievements as $achievement)
@@ -49,10 +50,11 @@
         </div>
     </div>
     @if(Auth::user()->accounts->dota_id == $users->accounts->dota_id)
-    <div class="w-1/6 ml-24 mt-2">
-        <a href="/players/{{$users->accounts->dota_id}}/achievements/create" class="bg-transparent hover:bg-indigo-700 text-indigo-800
-                            font-semibold hover:text-white py-2 px-4 border border-indigo-800
-                            hover:border-transparent rounded shadow-md text-lg">New Achievement</a>
+    <div class="w-1/6 ml-20 mt-16">
+        <a href="/players/{{$users->accounts->dota_id}}/achievements/create" class="bg-indigo-700 hover:bg-indigo-800 text-white
+                            font-semibold py-2 px-2 border border-indigo-800 hover:border-transparent rounded-lg shadow-md text-md
+                            flex items-center leading-loose"><i class="material-icons mr-1">
+                                add</i>New Achievement</a>
     </div>
     @endif
 @endsection
