5bdc43287323e3668fe11a0d290bfb56406bafd8
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-10-07 17:39:59 +0800

    route for noti

--- resources/views/notifications/reject_scrim.blade.php
+++ resources/views/notifications/reject_scrim.blade.php
@@ -1,6 +1,6 @@
 
 
         <a class="block text-default py-2 px-4 no-underline
-        text-md leading-loose ml-1 my-1 hover:bg-gray-200 border-b border-gray-400" href="{{ url('/tnotification') }}">
+        text-md leading-loose ml-1 my-1 hover:bg-gray-200 border-b border-gray-400" href="{{ url('/notifications') }}">
             Team {{ $noti->data['team_name']}} has rejected your invitation for scrim</a>
 
