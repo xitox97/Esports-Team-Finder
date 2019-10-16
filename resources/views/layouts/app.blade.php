c82d7dff2eb2766565fb2e6acb2255c97219dac2
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-10-16 20:48:25 +0800

    purge css jadi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="{{ asset('js/manifest.js') }}" defer></script>
+    <script src="{{ asset('js/vendor.js') }}" defer></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,39 @@
+// const mix = require('laravel-mix');
+
+// const tailwindcss = require('tailwindcss');
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .sass('resources/sass/app.scss', 'public/css')
+//     .options({
+//         processCssUrls: false,
+//         postCss: [tailwindcss('./tailwind.config.js')],
+//     });
 const mix = require('laravel-mix');
-
 const tailwindcss = require('tailwindcss');
+const glob = require('glob-all');
+const path = require('path');
+require('laravel-mix-purgecss');
 
-mix.js('resources/js/app.js', 'public/js')
+
+mix
+    .js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')],
+        cssNano: {
+            calc: false,
+        },
+        postCss: [
+            tailwindcss('./tailwind.config.js'),
+        ],
+    })
+    .purgeCss({
+        enabled: true,
+        paths: () => glob.sync([
+            path.join(__dirname, 'resources/views/**/*.blade.php'),
+            path.join(__dirname, 'resources/js/**/*.vue'),
+        ]),
+        extensions: ['html', 'js', 'php', 'vue'],
+        extractorPattern: /[\w-/:]+(?<!:)/g,
+        whitelist: ['pagination', 'page-link', 'page-item disabled', 'page-item active']
     });
