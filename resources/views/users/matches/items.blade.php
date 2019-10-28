1702d935ba8cec5ce77e48df5dc30cf1c5ea3116
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-10-28 21:11:59 +0800

    48%

--- resources/views/users/matches/items.blade.php
+++ resources/views/users/matches/items.blade.php
@@ -2,19 +2,59 @@
                     {{$player['item_3']}} {{$player['item_4']}} {{$player['item_5']}} --}}
 
 
-    @foreach($itemsData->items as $item)
+    @foreach($itemsData->items as $indexKey => $item)
+    @php
+        $random = rand();
+    @endphp
         @if($item['id'] == $player['item_0'])
-            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10">
+            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10 cursor-pointer" v-tooltip.bottom="{ html: '{{$indexKey}}{{$item['id']}}{{$random}}' }">
+
+            <div id="{{$indexKey}}{{$item['id']}}{{$random}}" class="tooltip-content">
+                <div class="p-2">
+                    <p>{{$item['dname']}}</p>
+                </div>
+            </div>
+
         @elseif($item['id'] == $player['item_1'])
-            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10">
+            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10 cursor-pointer" v-tooltip.bottom="{ html: '{{$indexKey}}{{$item['id']}}{{$random}}' }">
+            <div id="{{$indexKey}}{{$item['id']}}{{$random}}" class="tooltip-content">
+                <div class="p-2">
+                    <p>{{$item['dname']}}</p>
+                </div>
+            </div>
+
         @elseif($item['id'] == $player['item_2'])
-            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10">
+            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10 cursor-pointer" v-tooltip.bottom="{ html: '{{$indexKey}}{{$item['id']}}{{$random}}' }">
+            <div id="{{$indexKey}}{{$item['id']}}{{$random}}" class="tooltip-content">
+                <div class="p-2">
+                    <p>{{$item['dname']}}</p>
+                </div>
+            </div>
+
         @elseif($item['id'] == $player['item_3'])
-            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10">
+            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10 cursor-pointer" v-tooltip.bottom="{ html: '{{$indexKey}}{{$item['id']}}{{$random}}' }">
+            <div id="{{$indexKey}}{{$item['id']}}{{$random}}" class="tooltip-content">
+                <div class="p-2">
+                    <p>{{$item['dname']}}</p>
+                </div>
+            </div>
+
         @elseif($item['id'] == $player['item_4'])
-            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10">
+            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10 cursor-pointer" v-tooltip.bottom="{ html: '{{$indexKey}}{{$item['id']}}{{$random}}' }">
+            <div id="{{$indexKey}}{{$item['id']}}{{$random}}" class="tooltip-content">
+                <div class="p-2">
+                    <p>{{$item['dname']}}</p>
+                </div>
+            </div>
+
         @elseif($item['id'] == $player['item_5'])
-            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10">
+            <img src="http://cdn.dota2.com{{ $item['img'] }}" class="h-10 cursor-pointer" v-tooltip.bottom="{ html: '{{$indexKey}}{{$item['id']}}{{$random}}' }">
+            <div id="{{$indexKey}}{{$item['id']}}{{$random}}" class="tooltip-content">
+                <div class="p-2">
+                    <p>{{$item['dname']}}</p>
+                </div>
+            </div>
         @endif
+
     @endforeach
 
