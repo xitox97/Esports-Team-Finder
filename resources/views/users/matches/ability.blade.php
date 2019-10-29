71d75d5b86a94dfeb8f4128790a76e65dfc615ed
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-10-29 19:22:59 +0800

    setel tooltip

--- resources/views/users/matches/ability.blade.php
+++ resources/views/users/matches/ability.blade.php
@@ -27,12 +27,124 @@
     @endforeach --}}
 
     {{-- coding pendek --}}
+    @php
+            $random = rand();
+        @endphp
     @if(array_key_exists("img",$itemsData->abilities[$itemsData->ability_id[$ability]]))
-        @foreach($itemsData->abilities[$itemsData->ability_id[$ability]] as $key => $img)
-            @if($key == "img")
-            <td class="border-b border-gray-300 "><img src="http://cdn.dota2.com{{$img}}" class="w-10"></td>
-            @endif
-        @endforeach
+        @php
+            $ab = $itemsData->abilities[$itemsData->ability_id[$ability]];
+            //dd($ab);
+        @endphp
+            <td class="border-b border-gray-300 "><img src="http://cdn.dota2.com{{$ab['img']}}" class="w-10 cursor-pointer"
+                v-tooltip.bottom="{ html: '{{$random}}' }"></td>
+            <div id="{{$random}}" class="tooltip-content">
+                <div class="p-2">
+
+                <div class="flex items-center">
+                    <div>
+                        <img src="http://cdn.dota2.com{{$ab['img']}}" class="w-14">
+                    </div>
+                    <div class="ml-2">
+                            <p class="text-lg font-semibold tracking-wide">{{$ab['dname']}}</p>
+
+                    </div>
+                </div>
+                <div class="p-2 bg-gray-900">
+                    @if(array_key_exists("behavior",$ab))
+                    @if(is_array($ab['behavior']))
+                    <p class="text-medium">TARGET:
+                    @foreach($ab['behavior'] as $bh)
+                        @if($loop->last != true)
+                        {{$bh}} /
+                        @else
+                        {{$bh}}
+                        @endif
+                    @endforeach
+                    </p>
+                    @else
+                    <p class="text-medium">TARGET: {{$ab['behavior']}}</p>
+                    @endif
+                    @endif
+                    @if(array_key_exists("dmg_type",$ab))
+                        @if(is_array($ab['dmg_type']))
+                        <p class="text-medium">DAMAGE TYPE:
+                        @foreach($ab['dmg_type'] as $d)
+                            @if($loop->last != true)
+                            {{$d}} /
+                            @else
+                            {{$d}}
+                            @endif
+                        @endforeach
+                        </p>
+                        @else
+                        <p class="text-medium">DAMAGE TYPE: {{$ab['dmg_type']}}</p>
+                        @endif
+                    @endif
+                    @if(array_key_exists("bkbpierce",$ab))
+                    <p class="text-medium">PIERCES SPELL IMMUNITY: {{$ab['bkbpierce']}}</p>
+                    @endif
+                </div>
+                <div class="mt-2">
+                        <p class="text-sm italic text-gray-500">{{$ab['desc']}}</p>
+                </div>
+                <div class="mt-2">
+                    @foreach($ab['attrib'] as $att)
+
+                <p class="text-medium">{{$att['header']}}
+                    @if(is_array($att['value']))
+                        @foreach($att['value'] as $v)
+                        @if($loop->last != true)
+                        {{$v}} /
+                        @else
+                        {{$v}}
+                        @endif
+                        @endforeach
+                    @else
+                    {{$att['value']}}
+                    @endif
+                </p>
+
+                    @endforeach
+                </div>
+
+                <div class="flex mt-2">
+                    <img src="{{  asset('img/mana.png') }}" alt="">
+                    @if(array_key_exists("mc",$ab))
+                        @if(is_array($ab['mc']))
+                            @foreach($ab['mc'] as $m)
+                            @if($loop->last != true)
+                            {{$m}} /
+                            @else
+                            {{$m}}
+                            @endif
+                            @endforeach
+                        @else
+                            {{$ab['mc']}}
+                        @endif
+                    @endif
+                    @if(array_key_exists("cd",$ab))
+                    <img src="{{  asset('img/cd.png') }}" alt="" class="ml-4">
+                    @if(is_array($ab['cd']))
+                            @foreach($ab['cd'] as $c)
+                            @if($loop->last != true)
+                            {{$c}} /
+                            @else
+                            {{$c}}
+                            @endif
+                            @endforeach
+                        @else
+                        {{$ab['cd']}}
+                        @endif
+
+                    @endif
+                </div>
+
+            </div>
+
+            </div>
+        {{-- {{dd($img)}} --}}
+
+
     @else
         <td class="border-b border-gray-300">
         <img src="{{asset('img/talent.jpg')}}" class="w-10">
