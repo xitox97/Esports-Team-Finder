b103557db64bf23750df4f0618cdb31344a019c5
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-11-01 11:41:23 +0800

    65%

--- resources/views/teams/myteam.blade.php
+++ resources/views/teams/myteam.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="breadcrumb" class="ml-4 pt-2">
+<section id="breadcrumb" class="ml-4 pt-2 text-white font-medium tracking-wide">
     <span class="italic text-sm">Home / Team / <a href="/teams/{{$myTeam->id}}"
         class="no-underline hover:underline text-blue-500">{{$myTeam->name}}</a></span>
 </section>
 <div class="container ml-20 mt-12">
     <div class="flex flex-wrap">
         <div class="w-2/5">
-            <div class="flex flex-col bg-white shadow-lg rounded-lg mr-4">
-                <div class="w-full px-10 bg-purple-500   relative text-center pb-1">
-                        <span class="text-white font-semibold text-2xl capitalize text-center">Team {{$myTeam->name}}</span>
+            <div class="flex flex-col bg-dark-100 shadow-lg rounded-lg mr-4">
+                <div class="w-full px-10 bg-purple-600   relative text-center pb-1">
+                        <span class="text-white font-mono font-semibold text-2xl capitalize text-center">Team {{$myTeam->name}}</span>
                         @if(auth()->user()->id == $myTeam->captain_id)
                         <i class="material-icons cursor-pointer absolute ml-16 mb-1 text-white md-48"  v-click-outside="hides" v-on:click="dropTeam">
                                 more_horiz
                                 </i>
 
-                            <div v-show="team" id="dropdown" class="absolute rounded shadow right-0  bg-white w-4/12">
+                            <div v-show="team" id="dropdown" class="absolute rounded shadow right-0  bg-white w-4/12 font-mono">
                                 @if($myTeam->scrim == false)
                                 <a href="/teams/scrim/{{ $myTeam->id }}" class="block text-default py-2 px-4 no-underline hover:underline
                                     text-md leading-loose ml-1 my-1 hover:bg-gray-200"
@@ -47,10 +47,10 @@
                         <img src="{{  asset('storage/pics/' . $myTeam->image) }}" alt="" class=" rounded-sm w-3/5">
                 </div>
 
-                <div class="w-full px-10 bg-purple-500">
-                        <p class="font-semibold text-xl capitalize text-center text-white ">Team Information</p>
+                <div class="w-full px-10 bg-purple-600">
+                        <p class="font-semibold text-xl capitalize text-center text-white font-mono">Team Information</p>
                 </div>
-                <div class="flex flex-col  items-center mt-2 mx-10 mb-2">
+                <div class="flex flex-col  items-center mt-2 mx-10 mb-2 py-2 text-white font-mono">
                     <p class="text-center"><span class="font-semibold">City:</span> {{$myTeam->area}}</p>
                     <p class="text-center"><span class="font-semibold">State:</span> {{$myTeam->state}}</p>
                     <p class="text-center"><span class="font-semibold">Captain:</span> {{$myTeam->captain_id}}*</p>
@@ -68,10 +68,10 @@
                     <p class="text-center"><span class="font-semibold">Scrim:</span> Not Ready</p>
                     @endif
                 </div>
-                <div class="w-full px-10 bg-purple-500">
-                        <p class="font-semibold text-xl capitalize text-center text-white ">Achievements</p>
+                <div class="w-full px-10 bg-purple-600">
+                        <p class="font-semibold text-xl capitalize text-center text-white font-mono">Achievements</p>
                 </div>
-                <div class="flex flex-col  items-center mt-2 mx-10 mb-2">
+                <div class="flex flex-col  items-center mt-2 mx-10 mb-2 text-white font-mono  py-2">
                         <p class="text-left"><span class="font-semibold">Kl Major:</span> Top 3</p>
                         <p class="text-left"><span class="font-semibold">Ti 7:</span> top2*</p>
                 </div>
@@ -79,44 +79,44 @@
             </div>
         </div>
         <div class="w-2/4 rounded-lg">
-            <div class="flex flex-col justify-content bg-white shadow-lg ml-4">
-                    <div class="bg-purple-500 py-2">
+            <div class="flex flex-col justify-content bg-dark-100 shadow-lg ml-4 font-mono text-white">
+                    <div class="bg-purple-600 py-2">
                             <p class="text-white font-semibold text-2xl text-center">Player Roster</p>
                         </div>
                         <div>
                                 <table class="border-collapse w-full">
-                                        <thead class="text-gray-600">
-                                            <th class="capitalize border-b border-gray-300 py-4 text-center">ID</th>
-                                            <th class="capitalize border-b border-gray-300 py-4 text-center">Name</th>
-                                            <th class="capitalize border-b border-gray-300 py-4 text-center">Position</th>
-                                            <th class="capitalize border-b border-gray-300 py-4 text-center">Join Date</th>
-                                            <th class="capitalize border-b border-gray-300 py-4 "></th>
+                                        <thead class="text-gray-4 border-b border-gray-500">
+                                            <th class="capitalize py-4 text-center">ID</th>
+                                            <th class="capitalize py-4 text-center">Name</th>
+                                            <th class="capitalize py-4 text-center">Position</th>
+                                            <th class="capitalize py-4 text-center">Join Date</th>
+                                            <th class="capitalize py-4 "></th>
                                         </thead>
                                         <tbody class="text-center">
                                         @foreach($teamMembers as $teamMember)
 
-                                        <tr class="hover:bg-gray-200">
-                                        <td class="py-4 px-6 border-b border-gray-300">{{$teamMember->accounts->steam_name}}</td>
-                                        <td class="py-4 px-6 border-b border-gray-300 capitalize">{{$teamMember->name}}</td>
-                                        <td class="py-4 px-6 border-b border-gray-300">Mid*</td>
+                                        <tr class="hover:bg-content border-b border-gray-500 font-semibold">
+                                        <td class="py-4 px-6 ">{{$teamMember->accounts->steam_name}}</td>
+                                        <td class="py-4 px-6  capitalize">{{$teamMember->name}}</td>
+                                        <td class="py-4 px-6 ">Mid*</td>
                                         @foreach($teamMember->team as $t)
-                                        <td class="py-4 px-6 border-b border-gray-300">{{$t->pivot->created_at->format('d-m-Y')}}</td>
+                                        <td class="py-4 px-6 ">{{$t->pivot->created_at->format('d-m-Y')}}</td>
 
                                         @endforeach
-                                        <td class="py-4 px-6 border-b border-gray-300 flex">
-                                            <a href="{{ url('/players/' . $teamMember->accounts->dota_id) }}">
+                                        <td class="py-4 px-6  flex">
+                                            <a href="{{ url('/players/' . $teamMember->accounts->dota_id) }}" v-tooltip.top="'View Player'">
                                                 <i class="material-icons text-indigo-600 cursor-pointer md-48 hover:text-indigo-800">
                                                 search
                                                 </i>
                                             </a>
                                             @if(auth()->user()->id == $teamMember->id)
-                                            <a href="/leave/{{ $myTeam->id }}"><i class="material-icons  text-indigo-600 cursor-pointer md-48 hover:text-indigo-800">
+                                            <a href="/leave/{{ $myTeam->id }}" v-tooltip.top="'Kick Player'"><i class="material-icons  text-indigo-600 cursor-pointer md-48 hover:text-indigo-800">
                                             exit_to_app
                                             </i>
                                             </a>
                                             @endif
                                             @if(auth()->user()->id == $myTeam->captain_id and auth()->user()->id != $teamMember->id)
-                                        <a href="/kick/{{ $teamMember->id }}/team/{{ $myTeam->id}}"><i class="material-icons text-indigo-600 cursor-pointer md-48 hover:text-indigo-800">
+                                        <a href="/kick/{{ $teamMember->id }}/team/{{ $myTeam->id}}" v-tooltip.top="'Kick Player'"><i class="material-icons text-indigo-600 cursor-pointer md-48 hover:text-indigo-800">
                                                     close
                                                     </i></a>
                                             @endif
@@ -140,7 +140,7 @@
                             <svg class="fill-current h-6 w-6 text-orange-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
                         </span>
                     </div>
-            
+
             @elseif (session('scrim'))
             <div v-show="alert" class="mt-1 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" v-on:click="hideAlert" role="alert">
                 <strong class="font-bold">Ops!</strong>
--- resources/views/tournaments/index.blade.php
+++ resources/views/tournaments/index.blade.php
@@ -5,7 +5,7 @@
     <span class="italic text-sm">Home / Tournaments / <a href="tournaments" class="no-underline hover:underline text-blue-500">View</a></span>
     </section>
 
-    <div class="container ml-12 mt-12 pr-4 font-sans">
+    <div class="container ml-12 mt-12 pr-4 font-mono">
             @if (session('interest'))
             <div v-show="alert" class="-mt-8 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" v-on:click="hideAlert" role="alert">
                 <strong class="font-bold">Successfully!</strong>
@@ -25,23 +25,24 @@
             @endif
         <div class="flex flex-wrap flex-row justify-center">
             @foreach ($tournament as $tour)
-                <div class="max-w-xs rounded overflow-hidden shadow-lg mx-2 mb-2 mt-2  bg-dark-100  hover:bg-gray-100 ">
-                    <div>
-                            <img class="max-h-1/4 object-scale-down w-full" src="{{  asset('storage/tour/' . $tour->image) }}" alt="">
+                <div class="max-w-xs rounded rounded-t-none overflow-hidden shadow-lg mx-2 mb-2 mt-2  bg-dark-100  hover:bg-content border-t-4 border-purple-700">
+                    <div class="p-4">
+                            <div class="font-bold text-lg text-center text-white uppercase tracking-wide">{{ $tour->name }}</div>
+
+                            <img class="max-h-1/4 object-scale-down w-full rounded-lg mt-4" src="{{  asset('storage/tour/' . $tour->image) }}" alt="">
 
                     </div>
-                    <div class="px-6 py-4 text-center">
-                        <div class="font-bold text-xl">{{ $tour->name }}</div>
+                    <div class="px-6 py-4 text-center text-gray-300">
                         <p class="font-medium text-md mb-2">Prize Pool: RM {{ $tour->prizepool }}</p>
-                        <p class="text-gray-700 text-base"><span class="font-medium capitalize">Organizer:</span> {{ $tour->organizer }}</p>
-                        <p class="text-gray-700 text-base"><span class="font-medium capitalize">Date:</span> {{ $tour->start_date }} until {{ $tour->end_date }}</p>
-                        <p class="text-gray-700 text-base"><span class="font-medium capitalize">venue:</span> {{ $tour->venue }}</p>
-                        <p class="text-gray-700 text-base"><span class="font-medium capitalize">State:</span> {{ $tour->state }}</p>
+                        <p class="text-base"><span class="font-medium capitalize">Organizer:</span> {{ $tour->organizer }}</p>
+                        <p class="text-base"><span class="font-medium capitalize">Date:</span> {{ $tour->start_date }} until {{ $tour->end_date }}</p>
+                        <p class="text-base"><span class="font-medium capitalize">venue:</span> {{ $tour->venue }}</p>
+                        <p class="text-base"><span class="font-medium capitalize">State:</span> {{ $tour->state }}</p>
 
                         @if($tour->status == 1)
-                        <p class="text-gray-700 text-base"><span class="font-medium capitalize">Status:</span> Ended</p>
+                        <p class="text-base"><span class="font-medium capitalize">Status:</span> Ended</p>
                         @else
-                        <p class="text-gray-700 text-base"><span class="font-medium capitalize">Status:</span> Upcoming</p>
+                        <p class="text-base"><span class="font-medium capitalize">Status:</span> Upcoming</p>
                         @endif
                     </div>
                         @php
@@ -54,14 +55,16 @@
                                 }
                             }
                         @endphp
-                        <div class="text-center pb-3 -mt-3">
+                        <div class="text-center -mt-2 pb-6">
 
                         @if($check)
-                            <a href="/tournaments/notInterested/{{ $tour->id }}" class="inline-block bg-red-500 rounded-full
-                                px-3 py-1 text-md font-semibold text-white mt-3 text-center hover:bg-red-600">Cancel</a>
+                            <a href="/tournaments/notInterested/{{ $tour->id }}" class="inline-block bg-transparent rounded px-3 py-1
+                                text-md font-semibold text-white mt-3 text-center hover:bg-indigo-600 border-2 border-indigo-500
+                                hover:text-white tracking-wide">Cancel</a>
+
                         @else
-                        <a href="/tournaments/interested/{{ $tour->id }}" class="inline-block bg-green-500 rounded-full px-3 py-1
-                            text-md font-semibold text-white mt-3 text-center hover:bg-green-600">Interested</a>
+                        <a href="/tournaments/interested/{{ $tour->id }}" class="inline-block bg-indigo-500 rounded px-3 py-1
+                            text-md font-semibold text-white mt-3 text-center hover:bg-indigo-600 tracking-wide border-2 border-indigo-500">Interested</a>
                         @endif
                     </div>
 
