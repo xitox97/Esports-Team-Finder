828f4ea734627a30db1da80f635822011316eb61
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-11-06 21:26:31 +0800

    setel fixed medal

--- resources/views/users/medal.blade.php
+++ resources/views/users/medal.blade.php
@@ -18,12 +18,55 @@
         </div>
     </div>
 @else
-<div class="rankTierContainer">
-        <div class="rankMedal">
-            <img class="rankMedal-icon" src="
-            {{  asset('img/medal/rank_icon_' . $fetchPlayers->medal[0] . '.png') }}" alt="">
-            <img class="rankMedal-star" src="{{  asset('img/medal/rank_star_' . $fetchPlayers->medal[1] . '.png') }}" alt="">
 
+    @if($fetchPlayers->medal == 80)
+
+        @if($fetchPlayers->leaderboard_rank <= 1000 and $fetchPlayers->leaderboard_rank > 100)
+        <div class="rankTierContainer">
+            <div class="rankMedal">
+                <img class="rankMedal-icon" src="
+                {{  asset('img/medal/rank_icon_' . $fetchPlayers->medal[0] . '.png') }}" alt="">
+                <span class="text-orange-200 absolute self-center text-lg mt-10 font-semibold shadow-md">{{$fetchPlayers->leaderboard_rank}}</span>
+
+            </div>
         </div>
-    </div>
+        @elseif($fetchPlayers->leaderboard_rank <= 100 and $fetchPlayers->leaderboard_rank > 10)
+        <div class="rankTierContainer">
+            <div class="rankMedal">
+                <img class="rankMedal-icon" src="
+                {{  asset('img/medal/rank_icon_' . $fetchPlayers->medal[0] . 'b.png') }}" alt="">
+                <span class="text-orange-200 absolute self-center text-lg mt-10 font-semibold shadow-md">{{$fetchPlayers->leaderboard_rank}}</span>
+
+            </div>
+        </div>
+        @elseif($fetchPlayers->leaderboard_rank <= 10 and $fetchPlayers->leaderboard_rank > 1)
+        <div class="rankTierContainer">
+            <div class="rankMedal">
+                <img class="rankMedal-icon" src="
+                {{  asset('img/medal/rank_icon_' . $fetchPlayers->medal[0] . 'c.png') }}" alt="">
+                <span class="text-orange-200 absolute self-center text-lg mt-10 font-semibold shadow-md">{{$fetchPlayers->leaderboard_rank}}</span>
+
+            </div>
+        </div>
+        @elseif($fetchPlayers->leaderboard_rank == 1)
+        <div class="rankTierContainer">
+            <div class="rankMedal">
+                <img class="rankMedal-icon" src="
+                {{  asset('img/medal/rank_icon_' . $fetchPlayers->medal[0] . 'c.png') }}" alt="">
+                <span class="text-orange-200 absolute self-center text-lg mt-10 font-semibold shadow-md">{{$fetchPlayers->leaderboard_rank}}</span>
+
+            </div>
+        </div>
+        @endif
+    @else
+
+    <div class="rankTierContainer">
+            <div class="rankMedal">
+                <img class="rankMedal-icon" src="
+                {{  asset('img/medal/rank_icon_' . $fetchPlayers->medal[0] . '.png') }}" alt="">
+                <img class="rankMedal-star" src="{{  asset('img/medal/rank_star_' . $fetchPlayers->medal[1] . '.png') }}" alt="">
+
+            </div>
+        </div>
+    @endif
 @endif
