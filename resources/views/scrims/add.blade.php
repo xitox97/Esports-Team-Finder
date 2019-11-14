e6a3d0bbd47f2cfc69aede02ced5348cb1bd38b8
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-11-14 18:43:47 +0800

    scrim design

--- resources/views/scrims/add.blade.php
+++ resources/views/scrims/add.blade.php
@@ -1,46 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<section id="breadcrumb" class="ml-4 pt-2">
+<section id="breadcrumb" class="ml-4 pt-2 text-white font-medium tracking-wide">
     <span class="italic text-sm">Home / <a href="/scrims" class="text-blue-500 hover:underline">Scrim</a>  / Team
         <a href="/teams/{{ $team->id }}" class="text-blue-500 hover:underline">{{ $team->name }}</a></span>
 </section>
 {{-- //lg untuk laptop 1278 x XXX
 //xl tuk desktop 1440 x 737 --}}
-<div class="container ml-24 mt-12">
+<div class="container ml-24 mt-12 font-mono">
 
-        <p class="text-2xl  ml-1 font-bold  pb-4 capitalize text-indigo-800">Schedule Scrim</p>
-        <div class="flex justify-content">
-        <form method="POST" action="/scrims" class="bg-white shadow-lg w-5/12 rounded-lg pt-6 pb-4">
-            @csrf
-            <input name="team_id" type="hidden" value="{{ $myTeam->id }}">
-            <input name="opponent_id" type="hidden" value="{{ $team->id }}">
-            <div class="flex  flex-col justify-center items-center">
-            <div class="w-2/3 mb-3">
-                <label for="opponent" class="block text-gray-700 text-md font-bold mb-2">Opponent Team</label>
-                <input class="cursor-not-allowed appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200
-                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="name" id="name" value="{{$team->name}}" required disabled>
+                    <p class="text-2xl  ml-1 font-bold  pb-4 capitalize text-purple-600">Schedule Scrim</p>
+                    <div class="flex justify-content">
+                    <form method="POST" action="/scrims" class="bg-dark-100 shadow-lg w-5/12 rounded-lg pt-6 pb-4">
+                        @csrf
+                        <input name="team_id" type="hidden" value="{{ $myTeam->id }}">
+                        <input name="opponent_id" type="hidden" value="{{ $team->id }}">
+                        <div class="flex  flex-col justify-center items-center">
+                        <div class="w-2/3 mb-3">
+                            <label for="opponent" class="block text-white text-md font-bold mb-2">Opponent Team</label>
+                            <input class="cursor-not-allowed appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200
+                            rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="name" id="name" value="{{$team->name}}" required disabled>
+                        </div>
+                        <div class="w-2/3">
+                            <label for="date_time" class="block text-white text-md font-bold mb-2">Date & Time</label>
+                            <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200
+                            rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="datetime-local" name="date_time" id="date_time" required>
+                            @error('date_time')
+                                <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                            @enderror
+                        </div>
+
+                        <div class="flex flex-row justify-center items-center mt-3">
+                                <div class="mr-2">
+                                        <a  href="javascript:history.back()"  class="inline-block bg-transparent rounded px-4 py-1
+                                        text-md font-semibold text-white text-center hover:bg-indigo-600 border-2 border-indigo-500
+                                        hover:text-white tracking-wide">Cancel</a>
+                                </div>
+                                <button type="submit" class="btn-indigo text-white font-bold py-2 px-4 rounded-lg">
+                                        Invite
+                                </button>
+                        </div>
+
+                        </div>
+                    </form>
+
+
+                </div>
             </div>
-            <div class="w-2/3">
-                <label for="date_time" class="block text-gray-700 text-md font-bold mb-2">Date & Time</label>
-                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200
-                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="datetime-local" name="date_time" id="date_time" required>
-            </div>
 
-            <div class="flex flex-row justify-center items-center mt-3">
-                    <div class="mr-2">
-                            <a  href="javascript:history.back()"  class="bg-red-600 hover:bg-red-700 font-bold py-2 px-4 rounded-lg cursor-pointer text-white">Cancel</a>
-                    </div>
-                    <button type="submit" class="btn-indigo text-white font-bold py-2 px-4 rounded-lg">
-                            Invite
-                    </button>
-            </div>
-
-            </div>
-        </form>
-
-
-    </div>
 </div>
 
 
--- resources/views/scrims/scrimlist.blade.php
+++ resources/views/scrims/scrimlist.blade.php
@@ -1,55 +1,48 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Scrim list</div>
+<section id="breadcrumb" class="ml-4 pt-2 text-white font-medium tracking-wide">
+    <span class="italic text-sm">Home / Scrim Schedule</span>
+</section>
 
-                <div class="card" >
-
-                    <div class="card-body">
-                      <h5 class="card-title text-center"><b>Scrim list</b> </h5>
-                      <p class="card-text"></p>
-                    </div>
-
-                    <table class="table table-striped text-center">
-                            <thead>
-                              <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Team</th>
-                                <th scope="col">Time</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Action</th>
-
-                              </tr>
-                            </thead>
-                            <tbody>
-                            @foreach ($myTeam->scrims as $scrim)
-
-
-
-                                    <tr>
-                                      <td>{{$loop->index + 1}}</td>
-                                      <td> <a href="{{ url('/teams/' . $scrim->id) }}">{{$scrim->name}}</a> </td>
-                                      <td>{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $scrim->pivot->date_time)->format('h:i:s a') }}</td>
-                                      <td>{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $scrim->pivot->date_time)->format('d/m/Y') }}</td>
-                                      <td>
-                                        <a href="{{  url('/scrims/'. $scrim->pivot->id . '/details')  }}" class="btn btn-success"
-                                                role="button" aria-pressed="true">View Details</a></td>
-                                    </tr>
-
-
-                                  @endforeach
-                                </tbody>
-                          </table>
-
-                  </div>
-
-            </div>
-        </div>
-    </div>
+<div class="container ml-24 mt-12 font-mono">
+        <p class="text-2xl  ml-1 font-bold  pb-4 capitalize text-purple-600">Scrim list</p>
 </div>
+
+<div class="bg-dark-100 shadow-lg text-center rounded-lg px-4 pb-6 pt-5  max-w-4xl">
+        {{-- <p class="text-xl  ml-4 font-bold underline pb-2 uppercase">past achievements</p> --}}
+    <table class="border-collapse w-full table-fixed">
+        <thead class="text-white border-b border-gray-600">
+            <tr>
+                <th class="capitalize py-4 ">#</th>
+                <th class="capitalize py-4 ">Team</th>
+                <th class="capitalize py-4 ">Date</th>
+                <th class="capitalize py-4 ">Time</th>
+                {{-- <th class="capitalize py-4 "></th> --}}
+            </tr>
+
+        </thead>
+        <tbody class="text-center">
+        @forelse ($myTeam->scrims as $scrim)
+                <tr class="text-white font-medium border-b border-gray-600">
+                    <td class="py-4">{{$loop->index + 1}}</td>
+                    <td  class="py-4"> <a href="{{ url('/teams/' . $scrim->id) }}" class="no-underline hover:underline text-blue-500">{{$scrim->name}}</a> </td>
+                    <td  class="py-4">{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $scrim->pivot->date_time)->format('h:i:s a') }}</td>
+                    <td  class="py-4">{{ Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $scrim->pivot->date_time)->format('d/m/Y') }}</td>
+                    {{-- <td  class="py-4">
+                    <a href="{{  url('/scrims/'. $scrim->pivot->id . '/details')  }}" class="bg-pink-500 font-semibold text-white hover:bg-pink-600
+                        py-2 px-4 border border-pink-500 hover:border-pink-600 rounded">View Details</a></td> --}}
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4" class="py-4 px-6 border-b border-gray-300 text-center text-white"> <b> No Scheduled Scrims</b> </td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+</div>
+
+
+
 @endsection
 
