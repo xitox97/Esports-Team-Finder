698e547bf1a244dd2be7b1bc263e9ffb801203d4
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-12-15 22:50:33 +0800

    fixed notification alert

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -213,6 +213,61 @@
     @else
     <!-- if visit others profile !-->
     <div class="container w-full mx-auto">
+        @if (session('offer'))
+        <div class="flex flex-row-reverse mb-4">
+            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-green-700 bg-green-100 border border-green-400 rounded w-2/5" v-on:click="hideAlert" role="alert">
+                <strong class="font-bold">Success!</strong>
+                <span class="block sm:inline">{{ session('offer') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="w-6 h-6 text-green-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+        </div>
+        @endif
+        @if (session('pass'))
+        <div class="flex flex-row-reverse mb-4">
+            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-green-700 bg-green-100 border border-green-400 rounded w-2/5" v-on:click="hideAlert" role="alert">
+                <strong class="font-bold">Success!</strong>
+                <span class="block sm:inline">{{ session('pass') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg class="w-6 h-6 text-green-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+        </div>
+        @endif
+        @if (session('team'))
+        <div class="flex flex-row-reverse mb-4">
+            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-orange-700 bg-orange-100 border border-orange-400 rounded w-2/5" v-on:click="hideAlert" role="alert">
+                <strong class="font-bold">Hold up!</strong>
+                <span class="block sm:inline">{{ session('team') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="w-6 h-6 text-orange-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+        </div>
+        @endif
+        @if (session('captain'))
+        <div class="flex flex-row-reverse mb-4">
+            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-red-700 bg-red-100 border border-red-400 rounded w-2/5" v-on:click="hideAlert" role="alert">
+                <strong class="font-bold">Ops!</strong>
+                <span class="block sm:inline">{{ session('captain') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="w-6 h-6 text-red-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+        </div>
+        @endif
+        @if (session('full'))
+        <div class="flex flex-row-reverse mb-4">
+            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-red-700 bg-red-100 border border-red-400 rounded w-2/5" v-on:click="hideAlert" role="alert">
+                <strong class="font-bold">Ops!</strong>
+                <span class="block sm:inline">{{ session('full') }}</span>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg class="w-6 h-6 text-red-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                </span>
+            </div>
+        </div>
+        @endif
     <div class="px-10 pb-10 mx-64 mb-32 rounded-lg shadow-xl bg-dark-100">
         @if ( Auth::user()->id == $fetchPlayers->user_id )
         <div class="flex justify-center mx-10 ">
@@ -276,51 +331,7 @@
             </a>
                 </div>
             </div>
-        @if (session('offer'))
-            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-green-700 bg-green-100 border border-green-400 rounded" v-on:click="hideAlert" role="alert">
-                <strong class="font-bold">Success!</strong>
-                <span class="block sm:inline">{{ session('offer') }}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg class="w-6 h-6 text-green-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                </span>
-            </div>
-        @endif
-        @if (session('pass'))
-            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-green-700 bg-green-100 border border-green-400 rounded" v-on:click="hideAlert" role="alert">
-                <strong class="font-bold">Success!</strong>
-                <span class="block sm:inline">{{ session('pass') }}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg class="w-6 h-6 text-green-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                </span>
-            </div>
-        @endif
-        @if (session('team'))
-            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-orange-700 bg-orange-100 border border-orange-400 rounded" v-on:click="hideAlert" role="alert">
-                <strong class="font-bold">Hold up!</strong>
-                <span class="block sm:inline">{{ session('team') }}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg class="w-6 h-6 text-orange-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                </span>
-            </div>
-        @endif
-        @if (session('captain'))
-            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-red-700 bg-red-100 border border-red-400 rounded" v-on:click="hideAlert" role="alert">
-                <strong class="font-bold">Ops!</strong>
-                <span class="block sm:inline">{{ session('captain') }}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg class="w-6 h-6 text-red-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                </span>
-            </div>
-        @endif
-        @if (session('full'))
-            <div v-show="alert" class="relative px-4 py-3 -mt-8 text-red-700 bg-red-100 border border-red-400 rounded" v-on:click="hideAlert" role="alert">
-                <strong class="font-bold">Ops!</strong>
-                <span class="block sm:inline">{{ session('full') }}</span>
-                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg class="w-6 h-6 text-red-500 fill-current" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-                </span>
-            </div>
-        @endif
+
 
 
         <message-component></message-component>
