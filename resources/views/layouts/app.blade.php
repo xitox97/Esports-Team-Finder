8bd7024c6a54543e608b9ff90f7e95a19c1383ab
Farhan Hadi <farhan.abdhadi@gmail.com>
2019-12-30 22:01:05 +0800

    update fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
